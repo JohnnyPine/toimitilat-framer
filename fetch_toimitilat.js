2aa28a7911afe22655187de0f97ca3ce63e4457b
JohnnyPine <jobbis420@gmail.com>
2025-05-02 17:37:58 +0300

    Update fetch_toimitilat.js

--- fetch_toimitilat.js
+++ fetch_toimitilat.js
@@ -1,1 +1,19 @@
+const fs = require('fs');
+  const https = require('https');
 
+  const url = 'https://export.toimitilat.kauppalehti.fi/json/UMUK2ewKzTk1';
+
+  https.get(url, (res) => {
+    let data = '';
+
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      fs.writeFileSync('toimitilat.json', data);
+      console.log('Tiedot tallennettu toimitilat.json-tiedostoon.');
+    });
+  }).on('error', (err) => {
+    console.error('Virhe tietojen haussa:', err.message);
+  });
